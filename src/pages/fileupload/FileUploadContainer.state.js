6512a65353c48d779eee63920cb114f557b4988f
Brian Lagman <brianlagman@Brians-MBP.localdomain>
2019-11-12 14:19:55 -0800

    Update modal alert messages

--- src/pages/fileupload/FileUploadContainer.state.js
+++ src/pages/fileupload/FileUploadContainer.state.js
@@ -2,6 +2,31 @@ import React from 'react';
 import FileUpload from 'synapsefi-dev-ui/dist/components/FileUpload/FileUpload'
 import CheckboxGroup from 'synapsefi-dev-ui/dist/components/CheckboxGroup/CheckboxGroup'
 
+const modalButton = (props) => {
+  const {
+    isLoading,
+    updateModal
+  } = props;
+
+  return(
+    [
+      {
+        text: 'Cancel',
+        style: false ? 'isDisabled' : 'secondary',
+        onClick: () => updateModal(false),
+      },
+      {
+        text: 'Delete',
+        size: 'small',
+        style: false ? 'isLoading' : 'remove',
+        onClick: () => [alert('This is the removeDocument function, which would delete the selected files'), updateModal(false)]
+      }
+    ]
+  
+  )
+}
+
+
 const FileUploadContainer = () => {
   return(
     <FileUpload
@@ -23,34 +48,20 @@ const FileUploadContainer = () => {
         }
       }}
       deleteAction={() => alert('This is the deleteAction function, which would delete the file')}
-      fileUploadRowText={'1. Proof of account'}
+      fileUploadRowText={"1. Proof of account"}
       modalHeaderText={"Delete Document"}
       modalChildren={() => (
         <CheckboxGroup
           value={[]}
           propName="checkedFiles"
-          onChange={() => {console.log('Modal box checked')}}
+          onChange={() => alert("This is the updatedCheckedFiles function, which would check the selected files you would like to delete")}
           selectionType="classic"
           width="300px"
           options={[{key: "https://cdn.synapsepay.com/uploads/2019/10/02/3BmuL7.png", text: "2019/10/02/3B...png"}]}
         />
       )}
-      modalButtons={() => (
-        [
-          {
-            text: 'Cancel',
-            style: false ? 'isDisabled' : 'secondary',
-            onClick: () => console.log('Cancel'),
-          },
-          {
-            text: 'Delete',
-            size: 'small',
-            style: false ? 'isLoading' : 'remove',
-            onClick: () => console.log('Delete')
-          }
-        ]
-      )}
-      onDrop={() => alert('This is the onDrop function, which would be a function that updates the files')}
+      modalButtons={modalButton}
+      onDrop={() => alert('This is the onDrop function, which would update the files')}
     />
   )
 }
