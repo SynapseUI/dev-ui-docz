38836eccfad26452d90e5dcf2b267d403a2566e0
Brian Lagman <brianlagman@Brians-MBP.localdomain>
2019-11-12 10:23:34 -0800

    Clean up example File Upload component

--- src/pages/fileupload/FileUpload.state.js
+++ src/pages/fileupload/FileUpload.state.js
@@ -1,49 +1,22 @@
-import React, { Component } from 'react';
-import { requiredDocuments } from './fileUpload.data';
+import React from 'react';
 import styled from 'styled-components';
 import FileUploadContainer from './FileUploadContainer.state';
 import Colors from 'synapsefi-dev-ui/dist/colors';
 
-import { FileUploadRow, FileUploadStatus } from 'synapsefi-dev-ui';
-
 const Container = styled.div`
   border: 2px solid ${Colors.COOL_LIGHT};
   border-radius: 5px;
-  padding: 20px 30px 150px;
+  padding: 20px 30px 50px;
   height: auto;
   width: auto;
 `;
 
-class FileUpload extends Component {
-  render() {
-    return (
-      <Container>
-        {
-          // all the 1 - 4 starting with proof of account
-          _.map(requiredDocuments, (document, idx) => {
-            return (
-              <FileUploadContainer
-                key={idx}
-                document={document}
-                idx={idx}
-              />
-            )
-          })
-        }
-        <br/>
-        {/* Blank input thats grayed out */}
-        <FileUploadRow
-          // key={1}
-          idx={1}
-        >
-          {/* grayed out box of left hand side */}
-          <FileUploadStatus
-            fileValues={{}}
-          />
-        </FileUploadRow>
-      </Container>
-    );
-  }
+const FileUpload = () => {
+  return (
+    <Container>
+      <FileUploadContainer />
+    </Container>
+  )
 }
 
 export default FileUpload;
