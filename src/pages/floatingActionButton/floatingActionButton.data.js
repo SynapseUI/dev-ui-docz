6e7c67d06b2a4e9f4174c0c8618a0d87d3a0e7a9
Brian Lagman <brianlagman@Brians-MBP.localdomain>
2019-11-25 15:57:51 -0800

    Modified floating action button data

--- src/pages/floatingActionButton/floatingActionButton.data.js
+++ src/pages/floatingActionButton/floatingActionButton.data.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { SvgIcons } from 'synapsefi-dev-ui';
 
-const floatingActionButtonData = {
+let floatingActionButtonData = {
   Test1: {
     onClick: () => console.log('hi'),
     text: 'test 1',
-    icon: <SvgIcons.crypto_wallet size="24px" />
+    icon: <SvgIcons.crypto_wallet />
   },
   Test2: {
     onClick: () => console.log('hi'),
     text: 'test 2',
-    icon: <SvgIcons.deposit_account size="24px" />
+    icon: <SvgIcons.deposit_account />
   },
   Test3: {
     onClick: () => console.log('hi'),
     text: 'test 3',
-    icon: <SvgIcons.payment size="24px" />
-  },
+    icon: <SvgIcons.payment/>
+  }
 };
 
-export default floatingActionButtonData;
+export default floatingActionButtonData
